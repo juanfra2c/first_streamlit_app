d5849793dccf585ee76dd75ee82a485b17f4194e
juanfra2c <juanframnz@gmail.com>
2023-06-13 17:55:20 +0200

    Update streamlit_app.py
    Anado mas texto a la app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebarry Oatmeal')
+streamlit.text('Kale, spinach and Rocket')
+streamlit.text('Hard-Boiled Free range Egg')
