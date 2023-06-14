dc73b1ea2220c4d77dfc98831ab79e5d0be9bf79
juanfra2c <juanframnz@gmail.com>
2023-06-14 10:19:02 +0200

    Update streamlit_app.py
    Add Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebarry Oatmeal')
-streamlit.text('Kale, spinach and Rocket')
-streamlit.text('Hard-Boiled Free range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Bluebarry Oatmeal')
+streamlit.text('🥗 Kale, spinach and Rocket')
+streamlit.text('🐔 Hard-Boiled Free range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
